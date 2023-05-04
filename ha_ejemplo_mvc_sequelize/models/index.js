ce7a76adb8449f75e1efc0eca7ef4afeb3103444
Juan Andrés Chítaro <juanchitaro@gmail.com>
2023-05-04 14:48:39 -0300

    Cambios borrados

--- ha_ejemplo_mvc_sequelize/models/index.js
+++ ha_ejemplo_mvc_sequelize/models/index.js
@@ -1,12 +1,12 @@
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
-  blog_db, // Ej: hack_academy_db
-  root, // Ej: root
-  root, // Ej: root
+  process.env.DB_DATABASE, // Ej: hack_academy_db
+  process.env.DB_USERNAME, // Ej: root
+  process.env.DB_PASSWORD, // Ej: root
   {
-    host: "localhost", // Ej: 127.0.0.1
-    dialect: "mysql", // Ej: mysql
+    host: process.env.DB_HOST, // Ej: 127.0.0.1
+    dialect: process.env.DB_CONNECTION, // Ej: mysql
     logging: false, // Para que no aparezcan mensajes en consola.
   }
 );
