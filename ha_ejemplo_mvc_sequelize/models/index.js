c5546a5f5de28cd48c131a6757b53aae7cc41657
Juan Andrés Chítaro <juanchitaro@gmail.com>
2023-05-04 14:36:06 -0300

    index pronto

--- ha_ejemplo_mvc_sequelize/models/index.js
+++ ha_ejemplo_mvc_sequelize/models/index.js
@@ -1,14 +1,14 @@
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
-  process.env.DB_DATABASE, // Ej: hack_academy_db
-  process.env.DB_USERNAME, // Ej: root
-  process.env.DB_PASSWORD, // Ej: root
+  blog_db, // Ej: hack_academy_db
+  root, // Ej: root
+  root, // Ej: root
   {
-    host: process.env.DB_HOST, // Ej: 127.0.0.1
-    dialect: process.env.DB_CONNECTION, // Ej: mysql
+    host: "localhost", // Ej: 127.0.0.1
+    dialect: "mysql", // Ej: mysql
     logging: false, // Para que no aparezcan mensajes en consola.
-  },
+  }
 );
 
 const User = require("./User");
